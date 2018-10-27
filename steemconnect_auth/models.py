fd62709f9c3b274f691b841fa6fb0f6c570aff25
hakan çelik <hakancelik96@outlook.com>
2018-10-28 02:49:26 +0300

    steemconnect module added in models

--- steemconnect_auth/models.py
+++ steemconnect_auth/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# python steemconnect-client
+from steemconnect.client import Client
+
 class SteemConnectUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     refresh_token = models.CharField(max_length = 500,help_text = "steemconnect user code / to get get_refresh_token")
