2f4caf0d5cf9c7752742cd9576b2dd949477f921
hakan çelik <hakancelik96@outlook.com>
2019-02-03 20:27:20 +0300

    steem -> beem

--- steemconnect_auth/auth/steemconnect.py
+++ steemconnect_auth/auth/steemconnect.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.http import Http404
 
-# steem
-from steem.steem import Steem
+# beem
+from beem.account import Account
+from beem.exceptions import AccountDoesNotExistsException
 
 class SteemConnectBackend:
 
     def authenticate(self, request, username=None, **kwargs):
-        if Steem().get_account(username) is None:
+        try:
+            Account(username)
+        except AccountDoesNotExistsException:
             raise Http404
         user_model = get_user_model()
         user, created = user_model.objects.get_or_create(username=username)
