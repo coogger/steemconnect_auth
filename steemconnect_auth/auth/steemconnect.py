33e51c8c62ef2e142f488a394903458555b4eedc
hakan çelik <hakancelik96@outlook.com>
2019-02-09 02:30:35 +0300

    requests

--- steemconnect_auth/auth/steemconnect.py
+++ steemconnect_auth/auth/steemconnect.py
@@ -2,11 +2,13 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.http import Http404
-
+import requests
 
 class SteemConnectBackend:
 
     def authenticate(self, request, username=None, **kwargs):
+        if requests.get(f"https://steemit.com/@{username}").status_code != 200:
+            raise Http404
         user_model = get_user_model()
         user, created = user_model.objects.get_or_create(username=username)
         return user
